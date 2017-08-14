6a8a3fc7cd7ac46cd696f8e04509d63b499621ec
Daniel Everland <daniel@everlandgames.com>
2017-08-15 00:43:45 +0200

    Added some more dying effects

--- Assets/Scripts/Combat/DyingEffect.cs
+++ Assets/Scripts/Combat/DyingEffect.cs
@@ -8,20 +8,26 @@ public class DyingEffect : MonoBehaviour {
     public static PostProcessingProfile Profile { get; private set; }
 
     [SerializeField]
-    private PostProcessingProfile profile;
+    private PostProcessingProfile templateProfile;
     [SerializeField]
     private Creature player;
 
-    private const float EFFECT_THRESHOLD = 0.2f;
+    private const float EFFECT_THRESHOLD = 0.3f;
 
     private const float COLORGRADING_MIN = 0.5f;
     private const float COLORGRADING_MAX = 1;
+
+    private const float VIGNETTE_MIN = 0.1f;
+    private const float VIGNETTE_MAX = 0.4f;
+
+    private const float CHROMATIC_ABERRATION_MIN = 0f;
+    private const float CHROMATIC_ABERRATION_MAX = 0.4f; 
 
     private float oldHealth;    
     
     private void Awake()
     {
-        Profile = Instantiate(profile);
+        Profile = Instantiate(templateProfile);
     }
     private void Update()
     {
@@ -41,15 +47,37 @@ public class DyingEffect : MonoBehaviour {
     private void AssignValue(float percentage)
     {
         SetSaturation(percentage);
+        SetVignette(percentage);
+        SetChromaticAberration(percentage);
     }
     private void SetSaturation(float percentage)
     {
-        profile.colorGrading.enabled = percentage != 1;
-
         ColorGradingModel.Settings settings = Profile.colorGrading.settings;
         
         settings.basic.saturation = COLORGRADING_MIN + (COLORGRADING_MAX - COLORGRADING_MIN) * percentage;
 
         Profile.colorGrading.settings = settings;
+    }
+    private void SetVignette(float percentage)
+    {
+        Profile.vignette.enabled = percentage != 1;
+
+        VignetteModel.Settings settings = Profile.vignette.settings;
+
+        float invertedPercentage = 1 - percentage;
+        settings.intensity = VIGNETTE_MIN + (VIGNETTE_MAX - VIGNETTE_MIN) * invertedPercentage;
+
+        Profile.vignette.settings = settings;
+    }
+    private void SetChromaticAberration(float percentage)
+    {
+        Profile.chromaticAberration.enabled = percentage != 1;
+
+        ChromaticAberrationModel.Settings settings = Profile.chromaticAberration.settings;
+
+        float invertedPercentage = 1 - percentage;
+        settings.intensity = CHROMATIC_ABERRATION_MIN + (CHROMATIC_ABERRATION_MAX - CHROMATIC_ABERRATION_MIN) * invertedPercentage;
+
+        Profile.chromaticAberration.settings = settings;
     }
 }
