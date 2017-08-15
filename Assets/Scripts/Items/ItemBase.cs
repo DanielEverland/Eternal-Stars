7c0500fddcdc0708a0ad06e54612180b7d825d5a
Daniel Everland <daniel@everlandgames.com>
2017-08-15 14:17:40 +0200

    Made ItemBase abstract

--- Assets/Scripts/Items/ItemBase.cs
+++ Assets/Scripts/Items/ItemBase.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemBase : ScriptableObject {
+public abstract class ItemBase : ScriptableObject {
 
     public virtual Rarity Rarity { get { return _rarity; } }
     public virtual string Name { get { return _name; } }
