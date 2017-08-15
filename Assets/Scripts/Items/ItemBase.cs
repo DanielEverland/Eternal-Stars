2c4727013206ca46eebc761d244202962e4abe9d
Daniel Everland <daniel@everlandgames.com>
2017-08-15 03:11:10 +0200

    Removed test item from create asset menu

--- Assets/Scripts/Items/ItemBase.cs
+++ Assets/Scripts/Items/ItemBase.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Item.asset", menuName = "Items/TestItem", order = Utility.CREATE_ASSET_ORDER_ID)]
 public class ItemBase : ScriptableObject {
 
     public Rarity Rarity { get { return _rarity; } }
