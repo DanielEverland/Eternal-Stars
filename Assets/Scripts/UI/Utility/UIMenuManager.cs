02cd42585f147de470cfb766d72b5fd8fc981a44
Daniel Everland <daniel@everlandgames.com>
2017-08-18 15:56:18 +0200

    Added a UI menu manager

---
+++ Assets/Scripts/UI/Utility/UIMenuManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMenuManager : MonoBehaviour {
+
+    [SerializeField]
+    private List<MenuEntry> Entries;
+
+    private Dictionary<string, GameObject> InstantiatedMenus = new Dictionary<string, GameObject>();
+
+    private void Update()
+    {
+        for (int i = 0; i < Entries.Count; i++)
+        {
+            MenuEntry entry = Entries[i];
+
+            if (Keybindings.GetKeyUp(entry.keybinding))
+            {
+                ToggleMenu(entry);
+            }
+        }
+    }
+    private void ToggleMenu(MenuEntry entry)
+    {
+        if (InstantiatedMenus.ContainsKey(entry.keybinding))
+        {
+            Destroy(InstantiatedMenus[entry.keybinding]);
+            InstantiatedMenus.Remove(entry.keybinding);
+        }
+        else
+        {
+            GameObject obj = Instantiate(entry.prefab);
+            obj.transform.SetParent(Canvas2D.Instance.transform, false);
+            InstantiatedMenus.Add(entry.keybinding, obj);
+        }
+    }
+
+    [System.Serializable]
+    private struct MenuEntry
+    {
+        public string keybinding;
+        public GameObject prefab;
+    }
+}
