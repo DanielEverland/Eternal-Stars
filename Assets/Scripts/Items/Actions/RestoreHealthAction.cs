46b0638599b1bae47152fee6a75b170f7a47b516
Daniel Everland <daniel@everlandgames.com>
2017-08-22 02:09:35 +0200

    First iteration on proper item action serialization

--- Assets/Scripts/Items/Actions/RestoreHealthAction.cs
+++ Assets/Scripts/Items/Actions/RestoreHealthAction.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "RestoreHealthAction.asset", menuName = "Items/Actions/Restore Health", order = Utility.CREATE_ASSET_ORDER_ID)]
+[System.Serializable]
 public class RestoreHealthAction : ItemAction {
 
     [SerializeField]
