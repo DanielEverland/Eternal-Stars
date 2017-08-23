e5d39ab77b1ba7a7f05c9f7486ae6f08a8468fce
Daniel Everland <daniel@everlandgames.com>
2017-08-23 19:22:09 +0200

    Fixed error when an image using the ratio sustainer doesn't have a sprite reference

--- Assets/Scripts/UI/Utility/ImageRatioSustainer.cs
+++ Assets/Scripts/UI/Utility/ImageRatioSustainer.cs
@@ -18,6 +18,14 @@ public class ImageRatioSustainer : MonoBehaviour, ILayoutSelfController
     private float HeightRatio { get { return _image.sprite.rect.height / _image.sprite.rect.width;} }
     private float WidthRatio { get { return _image.sprite.rect.width / _image.sprite.rect.height; } }
     private RectTransform rectTransform { get { return (RectTransform)transform; } }
+
+    private bool hasReferences
+    {
+        get
+        {
+            return _image.sprite != null;
+        }
+    }
     
     private Vector2 AnchorSize
     {
@@ -32,6 +40,9 @@ public class ImageRatioSustainer : MonoBehaviour, ILayoutSelfController
 
     public void SetLayoutHorizontal()
     {
+        if (!hasReferences)
+            return;
+
         if (HeightIsSmallest)
         {
             rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.y * WidthRatio, AnchorSize.y);
@@ -39,6 +50,9 @@ public class ImageRatioSustainer : MonoBehaviour, ILayoutSelfController
     }
     public void SetLayoutVertical()
     {
+        if (!hasReferences)
+            return;
+
         if (WidthIsSmallest)
         {
             rectTransform.sizeDelta = new Vector2(AnchorSize.x, rectTransform.sizeDelta.x * HeightRatio);
